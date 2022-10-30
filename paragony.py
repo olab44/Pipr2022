aaa76a3d36b2a3f4ffd5d8691e7272c45f405174
ola <01178516@ow.edu.pl>
2022-10-30 16:36:50 +0100

    corrected types

--- paragony.py
+++ paragony.py
@@ -2,12 +2,22 @@ def split_price(price):
     price_zl=price//100
     price_gr=price%100
     return(price_zl, price_gr)
+
+
 def get_description (name,price):
     price_parts=(split_price(price))
     return f' price of {name} is {price_parts[0]}.{price_parts[1]:02}'
+
+def get_product():
+    name = input("enter product name: ")
+    price =input("enter product price: ") 
+    return name, int(price)
+
+product = get_product()
+
 def print_description (name,price): 
-    description=get_description (name,price)
+    description=get_product (name,price)
     print(description)
-    
-print_description('Oranges', 901)
+
+print_description(product[0], product[1])
 
