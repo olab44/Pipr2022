d07cd3b76020bfffa7748a6c0fed9d7727c44a7d
ola <01178516@ow.edu.pl>
2022-10-30 17:24:13 +0100

    added lists, function get_total_product

--- paragony.py
+++ paragony.py
@@ -3,21 +3,37 @@ def split_price(price):
     price_gr=price%100
     return(price_zl, price_gr)
 
-
 def get_description (name,price):
-    price_parts=(split_price(price))
+    price_parts= split_price(price)
     return f' price of {name} is {price_parts[0]}.{price_parts[1]:02}'
+
+def print_description (name,price): 
+    description=get_description (name,price)
+    print(description)
+
+# print_description('apples', 203)
 
 def get_product():
     name = input("enter product name: ")
-    price =input("enter product price: ") 
+    price =input("enter product price: ")
     return name, int(price)
 
 product = get_product()
+# print_description(product[0], product[1])
 
-def print_description (name,price): 
-    description=get_product (name,price)
-    print(description)
+receipt= [
+    ('apples', 233), ('bananas', 499), ('oranges', 802), ('milk', 312)
+]
+# receipt.append(get_product())
+# receipt.append(get_product())
+# receipt.append(get_product())
 
-print_description(product[0], product[1])
+for element in receipt:
+    print_description(element[0], element[1])
 
+def get_total_price():
+    for price in receipt:
+        price=price+receipt[price]
+        return price
+
+print(get_total_price)
