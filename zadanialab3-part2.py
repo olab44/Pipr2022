3faccf4ff432fdf3c0b781e2ea4f966243fd3dca
ola <01178516@ow.edu.pl>
2022-11-11 12:00:12 +0100

    added pre version of task3

--- zadanialab3-part2.py
+++ zadanialab3-part2.py
@@ -13,13 +13,28 @@ def counter(word):
 
 def word_frequency(phrase, frequency):
     new_phrase = []
-    word = phrase.split()
+    words = phrase.split()
     letter, amount = frequency
-    for word in phrase:
+    for word in words:
         amount_letters = counter(word)
         if letter not in amount_letters or amount_letters[letter] < amount:
             new_phrase.append(word)
-    return new_phrase
+    return " " .join(new_phrase)
 
 
 print(word_frequency("Alice in wonderland went into a deep coma.", ("e", 2)))
+
+
+def random_word_frequency(phrase, amount_of_krotki):
+    amount_of_krotki = [("a", 2), ("l", 3)]
+    new_phrase = []
+    pre_new_phrase = []
+    words = phrase.split()
+    for krotka in amount_of_krotki:
+        letter, amount = krotka
+        counter = word_frequency(phrase, krotka)
+        per_new_phrase.append(counter)
+    return new_phrase
+
+
+print(random_word_frequency("I literally can't deal with this drama right now.", [("a", 2), ("l", 3)]))
