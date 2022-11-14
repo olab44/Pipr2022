3f0ccd8bfc7d83e2083dbfbab4fa0635f0b76330
ola <01178516@ow.edu.pl>
2022-11-14 18:52:32 +0100

    corrected

---
+++ film3.py
@@ -0,0 +1,21 @@
+colors = []
+colors.append('red')
+colors.append('green')
+colors.append('blue')
+colors.remove('green')
+
+colors
+
+colours = ['red', 'blue', 'green', 'purple', 'yellow', 'white']
+some_colours = colours[0], colours[2], colours[4]
+some = colours[::2]
+
+for colour in colours:
+    print(colour)
+# receipt= []
+# receipt.append(get_product())
+# receipt.append(get_product())
+# receipt.append(get_product())
+
+countries = {'pl': 'Poland', 'us': 'United States of America'}
+countries['pl']
