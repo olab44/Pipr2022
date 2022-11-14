3bbb4dd0158b02df85727ebef8c504cc7e818ab5
ola <01178516@ow.edu.pl>
2022-11-14 18:47:12 +0100

    added functions: sum value, max value and checking key in dictionary

--- przed_zal1.py
+++ przed_zal1.py
@@ -17,6 +17,7 @@ if 'oranges' in dic_1:
     print(True)
 
 
+# słownik kwadratów liczb
 def squares(n):
     dictionary = {}
     for element in range(1, n+1):
@@ -27,10 +28,11 @@ def squares(n):
 print(squares(5))
 
 
+# znajdowanie największej wartości w słowniku
 def max_value(dict):
     list = []
     for element in dict:
-        list.append(element)
+        list.append(dict[element])
     return max(list)
 
 
@@ -38,3 +40,27 @@ dict = {
     'a': 5, 'b': 4, 'c': 2, 'd': 10, 'e': 4
 }
 print(max_value(dict))
+
+
+# sumowanie wartości w słowniku
+
+
+def sum_value(dict):
+    list = []
+    for element in dict:
+        list.append(dict[element])
+    return sum(list)
+
+
+print(sum_value(dict))
+
+
+# sprawdzanie czy istnieje klucz w słowniku
+def checking(key):
+    if key in dict:
+        return dict[key]
+    else:
+        return ("There is not such key in this dictionary")
+
+
+print(checking('5'))
