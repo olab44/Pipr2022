e09ee9a736e4d3008ce149c1c2eb02b749471aa2
ola <01178516@ow.edu.pl>
2022-11-14 18:53:13 +0100

    ff

--- zadanialab3-part2.py
+++ zadanialab3-part2.py
@@ -8,7 +8,7 @@ def counter(word):
     return word_counter
 
 
-# print(counter('tablet'))
+print(counter('tablet'))
 
 
 def word_frequency(phrase, frequency):
@@ -19,7 +19,7 @@ def word_frequency(phrase, frequency):
         amount_letters = counter(word)
         if letter not in amount_letters or amount_letters[letter] < amount:
             new_phrase.append(word)
-    return (new_phrase)
+    return " " . join(new_phrase)
 
 
 print(word_frequency("Alice in wonderland went into a deep coma.", ("e", 2)))
@@ -31,12 +31,25 @@ def random_word_frequency(phrase, amount_of_krotki):
     for krotka in amount_of_krotki:
         letter, amount = krotka
         for word in words:
-                letters = counter(word)
-                if letter not in letters or letters[letter] < amount:
-                    pass
-                else:
-                    words.remove(word)
-    return words
+            letters = counter(word)
+            if letter not in letters or letters[letter] < amount:
+                pass
+            else:
+                words.remove(word)
+    return " " . join(words)
 
 
 print(random_word_frequency("I literally can't deal with this drama right now.", [("a", 2), ("l", 3)]))
+
+
+def runner_up(n, arr):
+    list = []
+    for number in range(n+1):
+        for element in arr:
+            if element == max(arr):
+                list.append(element)
+                arr.remove(element)
+    return list[1]
+
+
+print(runner_up(4, [5, 4, 7, 8]))
