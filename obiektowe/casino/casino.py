5e7c94641d7289a25582e773e5a60df7eed9569d
Ola Buczma <Ola@macbook-pro-piotr.home>
2022-12-19 21:17:47 +0100

    implement casino

---
+++ obiektowe/casino/casino.py
@@ -0,0 +1,117 @@
+from random import randint
+
+
+class Casino:
+    def __init__(self, list_of_players=None):
+        '''class Casino. contains attribute:
+        :param representation: list_of_players
+        :param type: list
+        '''
+        if not list_of_players:
+            list_of_players = []
+        self.list_of_players = list_of_players
+
+    def add_player(self, new_player):
+        '''
+        adds player to the game
+        '''
+        self.list_of_players.append(new_player)
+        return self.list_of_players
+
+    def remove_player(self, old_player):
+        '''
+        removes player from the game
+        '''
+        if old_player not in self.list_of_players:
+            raise ValueError('chosen player is not on the list')
+        self.list_of_players.remove(old_player)
+        return self.list_of_players
+
+    def throw_dice(self):
+        '''
+        returns the list of 4 numbers- dices
+        '''
+        dices = []
+        for number in range(1, 5):
+            dices.append(randint(1, 6))
+        return dices
+
+    def game(self):
+        '''
+        each player gets his result,
+        returns list of tuples: (player, his result)
+        '''
+        results = []
+        for player in self.list_of_players:
+            player.dices = self.throw_dice()
+            pair = player.name, player.result
+            results.append(pair)
+        # return sorted(results, key=lambda results: results[1], reverse=True)
+        return results
+
+    def result_of_game(self):
+        '''
+        returns the winner of the game, or info that there is draw
+        '''
+        results = self.game()
+        sorted_results = sorted(results, key=lambda results: results[1], reverse=True)
+        winner, winning_score = sorted_results[0]
+        runner_up, second_score = sorted_results[1]
+        if winning_score == second_score:
+            return 'the game is unresolved'
+        else:
+            return f'{winner} won'
+
+
+class Player:
+    '''
+    Class Player. Contains attributes:
+    :param name: name
+    :param type: str
+    :param name: dices
+    :param type: list
+    :param name: result
+    :param type: int
+    '''
+    def __init__(self, name, dices=None, result=0):
+        if not name:
+            raise ValueError('name cannot be empty')
+        if not dices:
+            dices = []
+        self.name = name
+        self.dices = dices
+        self._result = result
+
+    def __eq__(self, other: 'Player') -> 'bool':
+        return self.name == other.name
+
+    @property
+    def result(self):
+        '''
+        gets the player's result, based on his dices
+        '''
+        possible_results = []
+        [a, b, c, d] = sorted(self.dices, reverse=True)
+        if a == b == c == d:
+            result_1 = a * 6
+            possible_results.append(result_1)
+        if (a * b * c * d) % 2 == 1:
+            result_2 = sum(self.dices) + 3
+            possible_results.append(result_2)
+        if a % 2 == 0 and b % 2 == 0 and c % 2 == 0 and d % 2 == 0:
+            result_3 = sum(self.dices) + 2
+            possible_results.append(result_3)
+        if a == b == c or b == c == d:
+            result_4 = b * 4
+            possible_results.append(result_4)
+        if a == b or b == c:
+            # pair is either a, b or b, c so in both cases result is, b * 2
+            result_5 = b * 2
+            possible_results.append(result_5)
+        if c == d:
+            result_6 = c * 2
+            possible_results.append(result_6)
+        else:
+            possible_results.append(0)
+        self._result = max(possible_results)
+        return self._result
---
+++ obiektowe/casino/test_casino.py
@@ -0,0 +1,129 @@
+from casino import Casino, Player
+import pytest
+
+
+def test_player_create():
+    player = Player('jane', [3, 3, 3, 3])
+    assert player.name == 'jane'
+    assert player.dices == [3, 3, 3, 3]
+    assert player.result == 18
+
+
+def test_player_create_empty_name():
+    with pytest.raises(ValueError):
+        Player('')
+
+
+def test_create_casino():
+    casino = Casino([
+        Player('jurek'), Player('mary'), Player('jane')
+    ])
+    assert casino.list_of_players == [Player('jurek'), Player('mary'), Player('jane')]
+
+
+def test_add_player():
+    casino = Casino([Player('james')])
+    assert casino.list_of_players == [Player('james')]
+    casino.add_player(Player('jonas'))
+    assert casino.list_of_players == [Player('james'), Player('jonas')]
+
+
+def test_remove_player():
+    casino = Casino([Player('james'), Player('jonas')])
+    casino.remove_player(Player('jonas'))
+    assert casino.list_of_players == [Player('james')]
+
+
+def test_throw_dice_the_same(monkeypatch):
+    def fours(a, b):
+        return 1
+    casino = Casino([Player('james')])
+    monkeypatch.setattr('casino.randint', fours)
+    assert casino.throw_dice() == [1, 1, 1, 1]
+
+
+def test_throw_dice_different_numbers(monkeypatch):
+    def fours(a):
+        return [3, 4, 5, 6]
+    casino = Casino([Player('john')])
+    monkeypatch.setattr('casino.Casino.throw_dice', fours)
+    assert casino.throw_dice() == [3, 4, 5, 6]
+
+
+def test_game_easy_version(monkeypatch):
+    def fours(a):
+        return [3, 3, 3, 6]
+    casino = Casino([Player('john'), Player('jane'), Player('mark')])
+    monkeypatch.setattr('casino.Casino.throw_dice', fours)
+    assert casino.game() == [('john', 12), ('jane', 12), ('mark', 12)]
+
+
+def test_game_harder_version(monkeypatch):
+    def three_of_kind(a):
+        return [3, 3, 3, 6]
+
+    def four_of_kind(b):
+        return [1, 1, 1, 1]
+
+    def pair(c):
+        return [6, 6, 2, 4]
+
+    casino = Casino([Player('john'), Player('jane'), Player('mark')])
+    # monkeypatch.setattr('casino.Casino.throw_dice', fours)
+    assert casino.game() == [('john', 12), ('jane', 18), ('mark', 10)]
+
+
+def test_game_results_with_winner(monkeypatch):
+    def this_game(a):
+        return [('john', 12), ('jane', 18), ('mark', 10)]
+    casino = Casino([Player('john'), Player('jane'), Player('mark')])
+    monkeypatch.setattr('casino.Casino.game', this_game)
+    assert casino.result_of_game() == 'jane won'
+
+
+def test_game_results_draw(monkeypatch):
+    def this_game(a):
+        return [('john', 10), ('jane', 10), ('mark', 8)]
+    casino = Casino([Player('john'), Player('jane'), Player('mark')])
+    monkeypatch.setattr('casino.Casino.game', this_game)
+    assert casino.result_of_game() == 'the game is unresolved'
+
+
+def test_player_results_four_of_kind():
+    player = Player('james', [4, 4, 4, 4])
+    assert player.result == 24
+
+
+def test_player_results_all_non_consecutive():
+    player = Player('james', [5, 3, 3, 1])
+    assert player.result == 15
+
+
+def test_player_results_all_consecutive():
+    player = Player('james', [4, 6, 6, 2])
+    assert player.result == 20
+
+
+def test_player_results_three_of_kind():
+    player = Player('james', [3, 3, 3, 1])
+    assert player.result == 13
+
+
+def test_player_results_pair_1():
+    player = Player('jurek', [4, 4, 3, 1])
+    assert player.result == 8
+
+
+def test_player_results_pair_2():
+    player = Player('jurek', [4, 3, 3, 1])
+    assert player.result == 6
+
+
+def test_player_results_pair_3():
+    player = Player('jurek', [6, 4, 3, 3])
+    assert player.result == 6
+
+
+def test_player_results_choosing_better():
+    player = Player('jurek', [6, 6, 3, 3])
+    assert player.result == 12
