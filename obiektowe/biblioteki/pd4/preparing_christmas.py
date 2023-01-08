c25aec4534a348b46979bbe9039fb37329a82ab0
Ola Buczma <Ola@macbook-pro-piotr.home>
2023-01-08 21:13:05 +0100

    counting total

--- obiektowe/biblioteki/pd4/preparing_christmas.py
+++ obiektowe/biblioteki/pd4/preparing_christmas.py
@@ -11,7 +11,7 @@ def read_from_file(file_handle):
         return kids
 
 
-class ElvesWorking:
+class Factory:
     def __init__(self, file_naughty_kids, file_brav_kids, elves, elves_on_l4=0):
         self._elves = elves
         self._elves_on_l4 = elves_on_l4
@@ -19,29 +19,48 @@ class ElvesWorking:
         self._file_naughty_kids = file_naughty_kids
         self._file_brav_kids = file_brav_kids
 
-    def read_from_file(self, file_handle):
-        with open(file_handle, 'r') as file_handle:
-            kids = []
-            for line in file_handle:
-                kids.append(line)
-            return kids
-
     def presents_for_naughty(self):
-        return len(self.read_from_file(self._file_naughty_kids))
+        return len(read_from_file(self._file_naughty_kids))
 
     def presents_for_brav(self):
-        return len(self.read_from_file(self._file_brav_kids))
+        return len(read_from_file(self._file_brav_kids))
+
+    def total_presents(self):
+        return self.presents_for_brav() + self.presents_for_naughty()
 
     def time_of_work_naughty(self):
         presents_for_naughty = self.presents_for_naughty()
-        time_in_hours = (0.5 * presents_for_naughty) / self._working_elves
+        time_in_minutes = (30 * presents_for_naughty) / self._working_elves
         # time_in_hours_brav = presents_for_naughty / working_elves * 2
-        return max(time_in_hours, 0.5)
+        return max(time_in_minutes, 30)
 
     def time_of_work_brav(self):
         presents_for_brav = self.presents_for_brav()
-        time_in_hours = presents_for_brav / (self._working_elves // 2)
-        return max(time_in_hours, 1)
+        time_in_minutes = presents_for_brav / (self._working_elves // 2) * 60
+        return max(time_in_minutes, 60)
+
+    # def count_total_time(self):
+    #     # first delegate elves for brav kids
+    #     # see if there are any elves left
+    #     # total_time_in_min = 0
+    #     if self._working_elves % 2 == 0:
+    #         total_time_in_minutes = self.time_of_work_brav() + self.time_of_work_naughty()
+    # #     # return total_time_in_minutes
+
+    # def time_for_naughty(self, elves):
+    #     time = (30 * self.presents_for_naughty()) / elves
+    #     return max(30, time)
+
+    # def time_for_brav(self, presents, elves):
+    #     time = presents / (elves // 2) * 60
+    #     return max(60, time)
+
+    # def total_time(self):
+    #     if self._working_elves % 2 == 1:
+    #         time_for_brav = self.time_of_work_brav()
+    #         if time_for_brav() >= self.time_for_naughty(1):
+
+
 
 
 print(read_from_file('naughty_kids.txt'))
--- obiektowe/biblioteki/pd4/test_preparing_christmas.py
+++ obiektowe/biblioteki/pd4/test_preparing_christmas.py
@@ -1,8 +1,8 @@
-from preparing_christmas import ElvesWorking
+from preparing_christmas import Factory, read_from_file
 
 
 def test_create_class():
-    factory = ElvesWorking('naughty_kids.txt', 'brav_kids.txt', 40)
+    factory = Factory('naughty_kids.txt', 'brav_kids.txt', 40)
     assert factory._elves == 40
     assert factory._elves_on_l4 == 0
     assert factory._working_elves == 40
@@ -12,12 +12,11 @@ def test_create_class():
 
 def test_read_from_file_naughty():
     naughty_kids = 'naughty_kids.txt'
-    factory = ElvesWorking('naughty_kids.txt', 'brav_kids.txt', 40)
     list_naughty = ['Lena Mason\n', 'Ross Fisher\n', 'Gregory Curtis\n',
                     'Jadon Schaefer\n', 'Alessandra Lara\n', 'Liam Cain\n',
                     'Chana Rocha\n']
 
-    assert factory.read_from_file(naughty_kids) == list_naughty
+    assert read_from_file(naughty_kids) == list_naughty
 
 
 def test_read_from_file_brav():
@@ -26,20 +25,34 @@ def test_read_from_file_brav():
                  'Simeon Roy\n', 'Kassandra Ferguson\n', 'Rylee Chang\n',
                  'Nick Wiggins\n', 'Hassan Mcconnell\n', 'Eden Harmon\n',
                  'Iyana Henderson\n', 'Johnathon Lutz\n', 'Laurel Savage\n']
-    factory = ElvesWorking('naughty_kids.txt', 'brav_kids.txt', 40)
-    assert factory.read_from_file(brav_kids) == list_brav
+    assert read_from_file(brav_kids) == list_brav
 
 
 def test_time_for_naughty():
-    factory = ElvesWorking('naughty_kids.txt', 'brav_kids.txt', 3)
-    assert factory.time_of_work_naughty() == 0.5
+    factory = Factory('naughty_kids.txt', 'brav_kids.txt', 4)
+    assert factory.time_of_work_naughty() == 52.5
 
 
 def test_presents_fot_naughty():
-    factory = ElvesWorking('naughty_kids.txt', 'brav_kids.txt', 40)
+    factory = Factory('naughty_kids.txt', 'brav_kids.txt', 40)
     assert factory.presents_for_naughty() == 7
 
 
 def test_presents_fot_brav():
-    factory = ElvesWorking('naughty_kids.txt', 'brav_kids.txt', 40)
+    factory = Factory('naughty_kids.txt', 'brav_kids.txt', 40)
     assert factory.presents_for_brav() == 12
+
+
+def test_time_for_brav_optimistic():
+    factory = Factory('naughty_kids.txt', 'brav_kids.txt', 40)
+    assert factory.time_of_work_brav() == 60
+
+
+def test_time_for_brav_typical():
+    factory = Factory('naughty_kids.txt', 'brav_kids.txt', 4)
+    assert factory.time_of_work_brav() == 360
+
+
+def test_count_total_time():
+    factory = Factory('naughty_kids.txt', 'brav_kids.txt', 5)
+    assert factory.count_total_time() == 412.5
